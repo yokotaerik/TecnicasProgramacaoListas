062cf76d7f8c35096245fbe92c34b84930551682
yokotaerik <yokota082003@gmail.com>
2024-08-16 16:15:02 -0300

    add padrao protoipo no telefone e add no teste

--- teste/index.ts
+++ teste/index.ts
@@ -1,5 +1,6 @@
 import Cliente from "../modelos/cliente";
 import Endereco from "../modelos/endereco";
+import Telefone from "../modelos/telefone";
 let cliente = new Cliente()
 cliente.nome = `Pedro de Alcântara João Carlos Leopoldo Salvador`
 cliente.nomeSocial = `Dom Pedro II`
@@ -13,6 +14,10 @@ endereco.estado = `Rio de Janeiro`
 endereco.pais = `Brasil`
 endereco.codigoPostal = `22220-000`
 cliente.endereco = endereco
+let telefone = new Telefone()
+telefone.ddd = `12`
+telefone.numero = `98155-2039`
+cliente.telefones.push(telefone)
 
 let dependente = new Cliente()
 dependente.nome = `Isabel Cristina Leopoldina Augusta Micaela`
@@ -21,6 +26,7 @@ dependente.dataCadastro = new Date(1921, 10, 14)
 dependente.dataNascimento = new Date(1846, 6, 29)
 dependente.endereco = (cliente.endereco.clonar() as Endereco)
 dependente.titular = cliente
+dependente.telefones.push(...cliente.telefones.map(telefone => telefone.clonar() as Telefone));
 cliente.dependentes.push(dependente)
 
 console.log(cliente);

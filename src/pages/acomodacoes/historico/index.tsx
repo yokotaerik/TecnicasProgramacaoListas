f7078041ee948a65cdcf7eeef47e692204c90847
yokotaerik <yokota082003@gmail.com>
2024-11-21 10:54:22 -0300

    atualizando

--- src/pages/acomodacoes/historico/index.tsx
+++ src/pages/acomodacoes/historico/index.tsx
@@ -1,32 +1,69 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type AcomodacoesInfos = {
+  tipoAcomodacao: string;
+  dataEntrada: string;
+  dataSaida: string;
+  hospedes: Hospede[];
+};
+
+type Hospede = {
+  nome: string;
+};
 
 const Acomodacoes: React.FC = () => {
-  const hospedagens = [1, 2, 3];
-  const tiposAcomodacao = [
-    "SolteiroSimples",
-    "CasalSimples",
-    "FamiliaSimples",
-    "FamiliaMais",
-    "SolteiroMais",
-    "FamiliaSuper",
-  ];
+  const [acomodacoes, setAcomodacoes] = useState<AcomodacoesInfos[]>([]);
+
+  // Mocke os dados
+  useEffect(() => {
+    setAcomodacoes([
+      {
+        tipoAcomodacao: "SolteiroSimples",
+        dataEntrada: "2021-09-01",
+        dataSaida: "2021-09-10",
+        hospedes: [{ nome: "João" }, { nome: "Maria" }, { nome: "José" }],
+      },
+      {
+        tipoAcomodacao: "CasalSimples",
+        dataEntrada: "2021-09-01",
+        dataSaida: "2021-09-10",
+        hospedes: [{ nome: "João" }, { nome: "Maria" }],
+      },
+    ]);
+  }, []);
+
   return (
     <div>
-      <h1 className="titulo_formulario">Acomodações</h1>
-      <button className="botao_comum"> <Link href={"/acomodacoes"}> Voltar </Link></button>
-      {tiposAcomodacao.map((tipo, index) => (
-        <div key={index}>
-          <div className="flex gap-5 items-center">
-            <h2 className="subtitulo">{tipo}</h2>
-            <button className="botao_comum">
-              <Link href={"/acomodacoes/cadastro"}>Cadastrar hospedagem</Link>
-            </button>
+      <div>
+        <h1 className="titulo_formulario">Histórico de acomodações </h1>
+        <div className="flex gap-2 justify-center">
+          <button className="botao_comum">
+            <Link href={"/acomodacoes/cadastro"}>Cadastrar hospedagem</Link>
+          </button>
+          <button className="botao_comum">
+            <Link href={"/acomodacoes"}>Acomodações ocupadas</Link>
+          </button>
+        </div>
+      </div>
+      {acomodacoes.map((a, index) => (
+        <div
+          key={index}
+          className="flex flex-col bg-slate-200 rounded-md p-2 mt-5"
+        >
+          <div className="flex gap-2">
+            <p className="texto_comum">{a.tipoAcomodacao}</p>
+            <p className="texto_comum">
+              Data check-in: {new Date(a.dataEntrada).toLocaleDateString()}
+            </p>
+            <p className="texto_comum">
+              Data check-out:{new Date(a.dataSaida).toLocaleDateString()}
+            </p>
           </div>
-          <ul>
-            {hospedagens.map((hospedagem, index) => (
-              <li key={index}>
-                <span className="texto_normal">{hospedagem}</span>
+          <ul className="flex gap-2">
+            {a.hospedes.map((hospede, index) => (
+              <li key={index} className="texto_comum">
+                {hospede.nome}
               </li>
             ))}
           </ul>
--- src/pages/acomodacoes/index.tsx
+++ src/pages/acomodacoes/index.tsx
@@ -2,31 +2,54 @@ import Link from "next/link";
 import React from "react";
 
 const Acomodacoes: React.FC = () => {
-  const hospedagens = [1, 2, 3];
-  const tiposAcomodacao = [
-    "SolteiroSimples",
-    "CasalSimples",
-    "FamiliaSimples",
-    "FamiliaMais",
-    "SolteiroMais",
-    "FamiliaSuper",
+  const acomodacoes = [
+    {
+      tipoAcomodacao: "SolteiroSimples",
+      dataEntrada: "2021-09-01",
+      dataSaida: "2021-09-10",
+      hospedes: [
+        { nome: "João" },
+        { nome: "Maria" },
+        { nome: "José" },
+      ],
+    },
+    {
+      tipoAcomodacao: "CasalSimples",
+      dataEntrada: "2021-09-01",
+      dataSaida: "2021-09-10",
+      hospedes: [
+        { nome: "João" },
+        { nome: "Maria" },
+      ],
+    },
   ];
+
   return (
     <div>
-      <h1 className="titulo_formulario">Acomodações ocupadas</h1>
-      <button className="botao_comum"> <Link href={"/acomodacoes/historico"}>  Histórico de acomodações</Link> </button>
-      {tiposAcomodacao.map((tipo, index) => (
-        <div key={index}>
-          <div className="flex gap-5 items-center">
-            <h2 className="subtitulo">{tipo}</h2>
-            <button className="botao_comum">
-              <Link href={"/acomodacoes/cadastro"}>Cadastrar hospedagem</Link>
-            </button>
+      <div>
+        <h1 className="titulo_formulario">Acomodações ocupadas</h1>
+        <div className="flex gap-2">
+          <button className="botao_comum">
+            <Link href={"/acomodacoes/cadastro"}>Cadastrar hospedagem</Link>
+          </button>
+          <button className="botao_comum">
+            <Link href={"/acomodacoes/historico"}>
+              Histórico de acomodações
+            </Link>
+          </button>
+        </div>
+      </div>
+      {acomodacoes.map((a, index) => (
+        <div key={index} className="flex flex-col gap-2 bg-slate-200 rounded-md p-2 mt-5">
+          <div className="flex gap-5">
+            <p className="texto_comum">{a.tipoAcomodacao}</p>
+            <p className="texto_comum">Data check-in: {new Date(a.dataEntrada).toLocaleDateString()}</p>
+            <p className="texto_comum">Data check-out:{new Date(a.dataSaida).toLocaleDateString()}</p>
           </div>
-          <ul>
-            {hospedagens.map((hospedagem, index) => (
-              <li key={index}>
-                <span className="texto_normal">{hospedagem}</span>
+          <ul className="flex gap-2">
+            {a.hospedes.map((hospede, index) => (
+              <li key={index} className="texto_comum">
+                {hospede.nome}
               </li>
             ))}
           </ul>
@@ -37,3 +60,4 @@ const Acomodacoes: React.FC = () => {
 };
 
 export default Acomodacoes;
+
